4714febe75a9d957dd89cfd97343b8f370fa83eb
BTurner15 <BTurner15@mail.greenriver.edu>
2019-01-11 09:34:47 -0800

    Code for Step 4 functions.php

--- functions.php
+++ functions.php
@@ -1,11 +1,24 @@
 <?php # functions.php
 /* Bruce Turner, IT 328, W2019 -->
    https://bturner.greenriverdev.com/328/includes/functions.php -->
-   01-09-19 Rev3.0
-   3.	Move your print function into an include file called functions.php. 
+   01-10-19 Rev.4
+   3. Move your print function into an include file called functions.php. 
         Add a function called largest() that takes an array as a parameter and 
         returns the largest value in the array. 
+   4. In your include file, define a function called average() that takes an 
+        array as a parameter and returns the average of the values in the function. 
+        Test the function from your index page.    
 */
+//----------------------------------------------------------------------------------
+//                          average()
+function average($numbers){
+       $numElements = count($numbers);
+       $sumOfElements = 0.0;
+       for ($i = 0; $i < $numElements; $i++) {
+          $sumOfElements += (double)$numbers[$i];
+       } 
+       return round($sumOfElements/(double)$numElements,2);
+} 
 //----------------------------------------------------------------------------------
 //                          largest()
 // Add a function called largest() that takes an array as a parameter and
@@ -23,19 +36,19 @@ function largest($numbers){
 // Create the error handler:
 function my_error_handler($e_number, $e_message, $e_file, $e_line, $e_vars) {
 
-	// Build the error message:
-	$message = "An error occurred in script '$e_file' on line $e_line: $e_message\n";
+  // Build the error message:
+  $message = "An error occurred in script '$e_file' on line $e_line: $e_message\n";
 
-	// Append $e_vars to  $message:
-	$message .= print_r ($e_vars, 1);
+  // Append $e_vars to  $message:
+  $message .= print_r ($e_vars, 1);
 
-	if (!LIVE) { // Development (print the error).
-		echo '<pre>' . $message . "\n";
-		debug_print_backtrace();
-		echo '</pre><br>';
-	} else { // Don't show the error.
-		echo '<div class="error">A system error occurred. We apologize for the inconvenience.</div><br>';
-	}
+  if (!LIVE) { // Development (print the error).
+    echo '<pre>' . $message . "\n";
+    debug_print_backtrace();
+    echo '</pre><br>';
+  } else { // Don't show the error.
+    echo '<div class="error">A system error occurred. We apologize for the inconvenience.</div><br>';
+  }
 
 } // End of my_error_handler() definition.
 //----------------------------------------------------------------------------------
--- index.php
+++ index.php
@@ -14,7 +14,10 @@
     3.	Move your print function into an include file called functions.php. 
         Add a function called largest() that takes an array as a parameter and 
         returns the largest value in the array. 
-        Test the function from your index page.    
+        Test the function from your index page.  
+    4.	In your include file, define a function called average() that takes an 
+        array as a parameter and returns the average of the values in the function. 
+        Test the function from your index page.        
 -->
 <!-- -->
 <html lang="en">
@@ -27,20 +30,22 @@
 </head>
 <body>
 
-<?php # set up error reporting
+<?php # 1st things first. Let's set up error reporting !
 
 // Flag variable for site status:
 define('LIVE', FALSE);
 include("includes/functions.php");
-// use my error handler:
+// Use my error handler:
 set_error_handler('my_error_handler');
-// start of file-specific code
+// start of the specific code
 // 2.	Define an array called $numbers, which contains 7, 9, 8, 9, 8, 8, 6. 
 $numbers = array(7,9,8,9,8,8,6);
 //      Print the array using your function.   
 printArr($numbers);
 $biggest = largest($numbers);
 echo "<br> Largest Value in array: $biggest";
+$result = average($numbers);
+echo "<br> Average of all Values in array: $result";
 ?>
 
 </body>
